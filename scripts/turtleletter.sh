bf4fa57148f1315528bd332ff5a46a882c6102f4
Bingham, Brian S <bsbingha@nps.edu>
2017-09-05 16:41:29 -0700

    letter ex

--- scripts/turtleletter.sh
+++ scripts/turtleletter.sh
@@ -1,1 +1,2 @@
-Test
+rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[4.0, 0.0, 0.0]' '[0.0, 0.0, 0.0]'
+rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[5.0, 0.0, 0.0]' '[0.0, 0.0, -4.5]'
